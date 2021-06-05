34232b85f449d7249038b384b043897faef7f2e8
jalbertomr <jalbertomr@yahoo.com>
2021-06-04 20:06:58 -0500

    Dependency Injection Using Field

--- src/main/java/com/bext/depinj/game/GameRunner.java
+++ src/main/java/com/bext/depinj/game/GameRunner.java
@@ -5,18 +5,19 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class GameRunner {
+	@Autowired
 	private GamingConsole game;
 /*
 	public GameRunner(GamingConsole game) {
 		System.out.println("using Constructor");
 		this.game = game;
 	}
-*/	@Autowired		
+		
 	public void setGame(GamingConsole game) {
 		System.out.println("using Setter");
 		this.game = game;
 	}
-	
+*/	
 	public void runGame() {
 		game.up();
 		game.down();
